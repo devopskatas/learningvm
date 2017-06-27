73d02a6d301c77f4c17dce9b61a8cd6a9260723f
Dave Swersky <dswersky@gmail.com>
2017-06-26 22:53:14 -0500

    fixed box name in Vagrantfile

--- learningvm/files/favorites.sh
+++ learningvm/files/favorites.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ shortcut-overlay false
 gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop']"
 firefox "http://www.devopskatas.com" &
