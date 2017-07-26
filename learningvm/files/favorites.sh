9a6035da17d727d8632580c28ba6edf3faabd6c4
DevOps Katas <dave@devopskatas.com>
2017-07-25 21:20:48 -0500

    updated firefox start page with utm source

--- learningvm/files/favorites.sh
+++ learningvm/files/favorites.sh
@@ -2,4 +2,4 @@
 
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ shortcut-overlay false
 gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop']"
-firefox "http://www.devopskatas.com" &
+firefox "http://www.devopskatas.com/?utm_source=lvm" &
