800f32b2045d863e34c76ee033afa5bcd74045df
DevOps Katas <dave@devopskatas.com>
2017-07-29 22:42:27 -0500

    Added sample chapter/icon, updated favorites.sh

--- learningvm/files/favorites.sh
+++ learningvm/files/favorites.sh
@@ -3,3 +3,12 @@
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ shortcut-overlay false
 gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop']"
 firefox "http://www.devopskatas.com/?utm_source=lvm" &
+sleep 5
+xdotool windowmove $(xdotool search 'DevOps Katas') 215 0 
+xdotool windowsize $(xdotool search 'DevOps Katas') 75% 100%
+
+if [ ! -f '/home/devops/Desktop/DevOps Katas Sample.desktop' ]; then
+    echo 'copying sample'
+    cp '/home/devops/DevOps Katas Sample.desktop' '/home/devops/Desktop/DevOps Katas Sample.desktop'
+    chmod +x 'Desktop/DevOps Katas Sample.desktop'
+fi
