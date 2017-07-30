92f9b6e2076021bb83bd5ad7219a548fdc0371d0
DevOps Katas <dave@devopskatas.com>
2017-07-29 22:43:02 -0500

    Added --name parameter to favorites.sh

--- learningvm/files/favorites.sh
+++ learningvm/files/favorites.sh
@@ -4,8 +4,8 @@ gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshel
 gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop']"
 firefox "http://www.devopskatas.com/?utm_source=lvm" &
 sleep 5
-xdotool windowmove $(xdotool search 'DevOps Katas') 215 0 
-xdotool windowsize $(xdotool search 'DevOps Katas') 75% 100%
+xdotool windowmove $(xdotool search --name 'DevOps Katas') 215 0 
+xdotool windowsize $(xdotool search --name 'DevOps Katas') 75% 100%
 
 if [ ! -f '/home/devops/Desktop/DevOps Katas Sample.desktop' ]; then
     echo 'copying sample'
