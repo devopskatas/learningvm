4fe373292e2d389631b4a6da57e7be74ce68eab6
DevOps Katas <dave@devopskatas.com>
2017-09-03 14:51:45 -0500

    various updates, consolidated files under "dk" folder

--- learningvm/files/dk/favorites.sh
+++ learningvm/files/dk/favorites.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ shortcut-overlay false
-gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop','/home/devops/DevOps Katas Sample.desktop']"
+gsettings set com.canonical.Unity.Launcher favorites "['gnome-terminal.desktop', 'firefox.desktop','gedit.desktop','/home/devops/dk/DevOps Katas Sample.desktop']"
 firefox "http://www.devopskatas.com/?utm_source=lvm" &
---
+++ learningvm/script/install-gogs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd dk
+curl -O https://cdn.gogs.io/0.11.29/linux_amd64.tar.gz
+tar -xzvf linux_amd64.tar.gz
+rm linux_amd64.tar.gz
