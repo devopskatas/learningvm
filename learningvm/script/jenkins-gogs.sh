de8731517f5aede666289f7acfa9cd38e570bdc1
Gogs <gogs@fake.local>
2017-10-02 21:32:31 -0500

    Jenkins Gogs script provisions Gogs, still need to push storelist repo

--- learningvm/script/jenkins-gogs.sh
+++ learningvm/script/jenkins-gogs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 curl -O https://cdn.gogs.io/0.11.29/linux_amd64.tar.gz
 mkdir -p jenkins-gogs
 tar -xzvf linux_amd64.tar.gz -C jenkins-gogs
@@ -12,4 +13,5 @@ sleep 5
 ./gogs admin create-user --name=cody.coder --password=katas --email=cody.coder@thecoders.com --admin=false
 cd ~/dk
 git clone https://github.com/devopskatas/web-storelist-k7.git
-curl -u devops:katas -H "Content-Type: application/json" -X POST -d '{"name":"web-storelist","description":"The Coder family Storelist app","private": false}' localhost:3000/api/v1/admin/users/cody.coder/repos
+curl -u devops:katas -H "Content-Type: application/json" -X POST -d '{"name":"web-storelist","description":"The Coder family Storelist app","private": false}' localhost:3000/api/v1/admin/users/cody.coder/repos 
+
