a65e91be04b6995025e23b9b0b2ec87d0ccdd0e8
DevOps Katas <dave@devopskatas.com>
2017-10-13 08:53:15 -0500

    Fixed learningvm build script, adding jenkins gogs server

--- learningvm/script/jenkins-gogs.sh
+++ learningvm/script/jenkins-gogs.sh
@@ -10,13 +10,14 @@ mkdir jenkins-gogs/gogs/custom
 mkdir jenkins-gogs/gogs/custom/conf
 cp app.ini jenkins-gogs/gogs/custom/conf/app.ini
 cd jenkins-gogs/gogs
-./gogs web &
+sudo -H -u devops ./gogs web &
 sleep 5
-./gogs admin create-user --name=devops --password=katas --email=devops@katas.com --admin=true
-./gogs admin create-user --name=cody.coder --password=katas --email=cody.coder@thecoders.com --admin=false
+sudo -H -u devops ./gogs admin create-user --name=devops --password=katas --email=devops@katas.com --admin=true
+sudo -H -u devops ./gogs admin create-user --name=cody.coder --password=katas --email=cody.coder@thecoders.com --admin=false
 cd ~/dk
 git clone https://github.com/devopskatas/web-storelist-k7.git
-curl -u devops:katas -H "Content-Type: application/json" -X POST -d '{"name":"web-storelist","description":"The Coder family Storelist app","private": false}' localhost:3000/api/v1/admin/users/cody.coder/repos 
+sudo -H -u devops curl -u devops:katas -H "Content-Type: application/json" -X POST -d '{"name":"web-storelist","description":"The Coder family Storelist app","private": false}' localhost:3000/api/v1/admin/users/cody.coder/repos 
 cd web-storelist-k7
 git remote set-url origin http://localhost:3000/cody.coder/web-storelist.git
 git push http://cody.coder:katas@localhost:3000/cody.coder/web-storelist.git
+sudo -H -u devops pkill gogs
